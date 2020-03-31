897f18789b4367531551fcce64934c87460a8688
Doups-bit <60627733+Doups-bit@users.noreply.github.com>
2020-03-31 12:17:19 -0600

    Delete type-search-index.js

--- PONGDemo3/doc/type-search-index.js
+++
@@ -1,1 +0,0 @@
-typeSearchIndex = [{"l":"All Classes","url":"allclasses-index.html"},{"p":"<Unnamed>","l":"Ball"},{"p":"<Unnamed>","l":"Game"},{"p":"<Unnamed>","l":"KeyInput"},{"p":"<Unnamed>","l":"Paddle"},{"p":"<Unnamed>","l":"PaddleHorizontal"},{"p":"<Unnamed>","l":"PongMain"},{"p":"<Unnamed>","l":"Window"}]
